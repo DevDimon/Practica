34623bb8a4b713afb009f3004bc0f7ab611a99e0
DevDimonLocal <devdimonpok@gmail.com>
2023-06-01 21:33:21 +0300

    реализована функция printMatrix

--- practica.cpp
+++ practica.cpp
@@ -5,6 +5,18 @@
 
 using namespace std;
 
+void printMatrix(int n, float *m)
+{
+    for (int s = 0; s < n; s++)
+    {
+        for (int c = 0; c < n + 1; c++)
+        {
+            cout << m[s*(n+1)+c] << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main(void)
 {
     int n, s, c;
@@ -24,13 +36,8 @@ int main(void)
         }
     }
     cout << "\n";
-
-    for (s = 0; s < n; s++)
-    {
-        for (c = 0; c < n + 1; c++)
-        {
-            cout << matrix[s][c] << " ";
-        }
-        cout << "\n";
-    }
+    
+    printMatrix(n, &matrix[0][0]);
 }
+
+
