902261bf18511e517c30ae4b7f0fbd2d6b66a158
DevDimonLocal <devdimonpok@gmail.com>
2023-06-02 19:33:11 +0300

    добавлена getValue

--- practica.cpp
+++ practica.cpp
@@ -5,16 +5,26 @@
 
 using namespace std;
 
+float getValue(int s, int c, int n, float *m)
+{
+    return  m[s*(n+1)+c];
+}
+
 void printMatrix(int n, float *m)
 {
     for (int s = 0; s < n; s++)
     {
         for (int c = 0; c < n + 1; c++)
         {
-            cout << m[s*(n+1)+c] << " ";
+            cout << getValue(s, c, n, &m[0]) << " ";
         }
         cout << "\n";
     }
+}
+
+void setFirstLine(int n, float *m)
+{
+    
 }
 
 int main(void)
